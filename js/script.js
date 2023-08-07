05c83ea204d0e5116b7e42c3e9f31597c1eeb71c
Imam <immomm02@gmail.com>
2023-08-08 00:18:11 +0300

    last commit of first practice

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -7,4 +7,10 @@ const personalMovieDB = {
 	genres: [],
 	privat: false	
 };
-
+const a = prompt ('Один из последних просмотренных фильмов?', '');
+const b = prompt ('На сколько вы его оцените?', '');
+const c = prompt ('Один из последних просмотренных фильмов?', '');
+const d = prompt ('На сколько вы его оцените?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
